a37a83c1137a36b726516d199a4b4333ab6ec32f
tanviway48 <tanvi.waykar04@gamil.com>
2025-08-04 13:14:08 +0530

    Initial commit: MERN Auth in progress

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+// import { connect } from "mongoose";
+import connectDB from './config/mongodb.js'
+import authRouter from './routes/authRoutes.js'
+ 
+
+const app=express();
+const port= process.env.Port || 4000
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials:true}))
+
+// API end point 
+app.get('/',(req,res)=>res.send("API working"))
+app.use('/api/auth',authRouter)
+
+app.listen(port,()=> console.log(`Server started on port:${port}`));
